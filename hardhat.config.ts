3b1f991ccb03d90da017c23f720ead2833828847
chongkw <chongkw@phillip.com.sg>
2024-05-30 12:05:25 +0800

    re-deploy factory contract

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,14 +1,12 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 
-const { INFURA_API_KEY, PRIVATE_KEY } = process.env;
-
 module.exports = {
   solidity: "0.8.26",
   networks: {
     sepolia: {
-      url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
-      accounts: [`${PRIVATE_KEY}`],
+      url: `https://sepolia.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts: [`0x${process.env.PRIVATE_KEY}`],
     },
   },
 };
---
+++ scripts/deploy.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+const { ethers } = require("hardhat");
+
+async function main() {
+  const [deployer] = await ethers.getSigners();
+
+  console.log("Deploying contracts with the account:", deployer.address);
+
+  const Factory = await ethers.getContractFactory("Factory");
+  await Factory.deploy();
+
+  console.log("Factory contract is being deployed...");
+
+  console.log("Factory deployed to:", deployer.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
