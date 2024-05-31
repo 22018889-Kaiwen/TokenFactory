2ce416466766c786eaf6e0613f43e92bfafc1c91
chongkw <chongkw@phillip.com.sg>
2024-05-31 09:24:11 +0800

    update deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,4 +1,3 @@
-require("dotenv").config();
 const { ethers } = require("hardhat");
 
 async function main() {
@@ -6,12 +5,45 @@ async function main() {
 
   console.log("Deploying contracts with the account:", deployer.address);
 
+  //Deploying Factory contract
   const Factory = await ethers.getContractFactory("Factory");
   await Factory.deploy();
 
   console.log("Factory contract is being deployed...");
 
-  console.log("Factory deployed to:", deployer.address);
+  console.log("Factory deployed");
+
+
+  //Deploying Token contract
+  const Token = await ethers.getContractFactory("Token");
+
+  const tokenName = "CATCOIN";
+  const tokenSymbol = "CAT";
+  const tokenDecimals = 18;
+  const tokenTotalSupply = ethers.parseUnits("100", tokenDecimals);
+  const ownerAddress = deployer.address;
+  const factoryAddress = deployer.address;
+
+  console.log(
+    "Deploying with parameters:",
+    tokenName,
+    tokenSymbol,
+    tokenDecimals,
+    tokenTotalSupply.toString(),
+    ownerAddress,
+    factoryAddress
+  );
+
+  await Token.deploy(
+    tokenName,
+    tokenSymbol,
+    tokenDecimals,
+    tokenTotalSupply,
+    ownerAddress,
+    factoryAddress
+  );
+
+  console.log("Contract deployed");
 }
 
 main()
