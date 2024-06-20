395d955e97c77030f7abf67f43bce2452782b04c
chongkw <chongkw@phillip.com.sg>
2024-06-20 12:32:27 +0800

    fix: updated hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,12 +1,33 @@
+import { vars } from "hardhat/config";
+import * as fs from "fs";
+import * as path from "path";
+import { ApiBaseUrl } from "@fireblocks/fireblocks-web3-provider";
+
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
+
+const INFURA_API_KEY = vars.get("INFURA_API_KEY");
+const FIREBLOCKS_API_KEY = vars.get("FIREBLOCKS_API_KEY");
+const MNEMONIC = vars.get("MNEMONIC");
+const apiSecret = fs.readFileSync(
+  path.resolve(
+    "/home/kai/dev/Tokenfactory/hardhat-project/fireblocks_secret.key"
+  ),
+  "utf8"
+);
 
 module.exports = {
   solidity: "0.8.26",
   networks: {
     sepolia: {
-      url: `https://sepolia.infura.io/v3/${process.env.INFURA_API_KEY}`,
-      accounts: [`0x${process.env.PRIVATE_KEY}`],
+      url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: { mnemonic: MNEMONIC },
+      fireblocks: {
+        apiBaseUrl: ApiBaseUrl.Sandbox,
+        privateKey: apiSecret,
+        apiKey: FIREBLOCKS_API_KEY,
+        vaultAccountIds: 1,
+      },
     },
   },
 };
